73150f6ae7fb1e8f7bbedd27b9e317390f4dbd65
Tan <jantan@8c85904778c3.ant.amazon.com>
2018-10-06 09:14:39 +0800

    Added config

---
+++ frontend/js/config.js
@@ -0,0 +1,8 @@
+var configuration = {
+    CognitoUserPool: {
+        UserPoolId: "eu-west-1_7Iqh5yQvs",
+        ClientId: "3808f1f4vb8ed65i3ibvvvvf19"
+    },
+    ApiGatewayEndpoint: "https://frxlknwtpb.execute-api.eu-west-1.amazonaws.com/prod",
+    DisplayAttributeName: "given_name"
+}
